a77c7d3846d3e58c7a6ee1380cefe281fb22a70a
Dor Lazarov <lazamal@gmail.com>
2026-03-28 23:37:59 +0300

    removed i

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -132,9 +132,7 @@ function App() {
             <span className="block md:inline">
               Know which skills you're missing{" "}
             </span>
-            <span className="block md:inline">
-              <i>before</i> you apply.
-            </span>
+            <span className="block md:inline">before you apply.</span>
           </p>
         </div>
       </header>
